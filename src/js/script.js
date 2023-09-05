86a88c1636fd4f4cd648df23f40aa7833586d2e6
Alexey <zalexeyv@gmail.com>
2023-09-05 16:41:54 +0300

    input_to_javascript-end

---
+++ src/js/script.js
@@ -0,0 +1,72 @@
+var name = "Ivan";
+
+let number = 7;
+const pi = 3.14;
+
+number = 4;
+
+let leftBorderWidth;
+
+// number
+// string - "", '', ``
+// true/false
+// null - не существует
+// undefined - существует но не заполнено
+/* let obj = {
+	name: 'apple',
+	color: 'green',
+	weight: 200
+} */
+// Symbol
+
+// alert(1234)
+// console.log(number);
+/* let answer = confirm ("Вам есть 18?");
+console.log(answer); */
+
+/* let answer = prompt("Вам есть 18?", "");
+console.log(answer); */
+
+// console.log(4 + 'fdd');
+
+// let isChecked = true,
+//     isClosed = true;
+
+// console.log(isChecked && isClosed); /* оператор И */
+
+// console.log(isChecked || isClosed); /* оператор или */
+
+// if (2*5 == 8*1) {
+// 	console.log('Верно')
+// } else {
+// 	console.log('Ошибка')
+// }
+
+// let answer = confirm ("Вам есть 18?");
+// if (answer) {
+// 	console.log('Прозодите')
+// } else {
+// 	console.log('Уходи')
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+// 	console.log('Неправильно')
+// } else if (num > 100) {
+// 	console.log('Много')
+// } else {
+// 	console.log('Верно')
+// }
+
+// for(let i = 1; i < 8; i++) {
+// 	console.log(i);
+// }
+
+function logging(a, b) {
+	console.log( a + b )
+}
+
+logging(3, 5);
+
+logging(6, 8);
