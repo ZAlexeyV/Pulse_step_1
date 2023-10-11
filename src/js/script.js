567687ca48a6c30cb5ee7d682f69c4631613ad34
Alexey <zalexeyv@gmail.com>
2023-10-11 19:40:46 +0300

    step_7_end

--- src/js/script.js
+++ src/js/script.js
@@ -127,4 +127,36 @@ $(document).ready(function(){
 		})
 	});
 
+	function valideForms(form){
+		$(form).validate({
+			rules: {
+				name: {
+					required: true,
+					minlength: 2
+				  },
+				phone: "required",
+				email: {
+					required: true,
+					email: true
+				}
+			},
+			messages: {
+				name: {
+					required: "*Пожалуйста, введите свое имя",
+					minlength: jQuery.validator.format("*Минимум {0} символа")
+				  },
+				phone: "*Пожалуйста, введите свой номер телефона",
+				email: {
+				  required: "*Пожалуйста, введите свою почту",
+				  email: "*Неправильно введен адрес почты"
+				}
+			}
+		});
+	};
+
+	valideForms('#consultation-form');
+	valideForms('#consultation form');
+	valideForms('#order form');
+
+	$('input[name=phone]').mask("+7 (999) 999-9999");
 });
